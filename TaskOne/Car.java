9f1e53e2fef5d0e9bfd28c453cd562be282421a4
Joseph <120279816+JJuarez00@users.noreply.github.com>
2023-09-19 20:46:52 -0400

    Create Car.java

---
+++ TaskOne/Car.java
@@ -0,0 +1,64 @@
+package modulethree.taskOne;
+
+public class Car {
+    private int modelYear;
+    private String modelName;
+    private String bodyShape;
+    private int milesDriven;
+    private String carColor;
+
+
+// -  -  -  - Getter methods
+  
+    public int getModelYear() {               //  Get YEAR
+        return modelYear;
+    }
+
+    public String getModelName() {           // Get NAME
+        return modelName;
+    }
+    
+    public String getBodyShape() {            // Get SHAPE
+        return bodyShape;
+    }
+
+    public int getMilesDriven() {              // Get MILES
+        return milesDriven;
+    }
+
+    public String getCarColor() {               // Get COLOR
+        return carColor;
+    }
+
+
+
+  
+// -  -  -  - Setter methods
+  
+    public void setModelYear(int modelYear) {               // Set YEAR
+        this.modelYear = modelYear;
+    }
+
+    public void setModelName(String modelName) {     // Set NAME
+        this.modelName = modelName;
+    }
+    
+    
+    public void setBodyShape(String bodyShape) {         // Set BODY
+        this.bodyShape = bodyShape;
+    }
+
+    public void setMilesDriven(int milesDriven) {           // Set MILES
+        this.milesDriven = milesDriven;
+    }
+
+    public void setCarColor(String carColor) {               // Set COLOR
+        this.carColor = carColor;
+    }
+
+    @Override
+    public String toString() {
+        return "Car Information:\n Model Year: " + modelYear + "\nModel Name" + modelName
+                                             + "\n Mile Driven: " + milesDriven + "\nCar Color: " + carColor + "\n"; 
+}
+}
