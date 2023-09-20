88bedc94a4e5e42626ef27f9ef9ca6afac84b5bd
Joseph <120279816+JJuarez00@users.noreply.github.com>
2023-09-19 20:58:02 -0400

    Update One.java

--- TaskOne/One.java
+++ TaskOne/One.java
@@ -1,5 +1,5 @@
 /*
-Chapter 9: 
+ Chapter 9: 
                   A class is a datatype so we would categorize it with things like int, boolean, string, and so on.
                   It's just a better data type designed to fit our needs explicitly.
                   Choose one of the items on the list below and create a class to represent the object as a single digital datatype:
@@ -8,9 +8,9 @@ Chapter 9:
                                               -  and override the toString() method to get full points
                   Then use a Java main method to instantiate a couple unique instances of the class and print them to the console.
 
-Note:
-- for this assignment I'm going to get car information
 
+Note:
+- for this assignment im going to get car information
 */
 
 package modulethree.taskOne;
@@ -23,16 +23,18 @@ public class One {
         Car car1 = new Car();
         car1.setModelYear(2013);
         car1.setModelName("BMW 328i xDrive");
+        car1.setBodyShape("Sedan");
         car1.setMilesDriven(56632);
         car1.setCarColor("Black");
 
         
         /* Another car but im just going to take it out but
-           if you want you and remove the comment thingy.
+             if you want you and remove the comment thingy.
         
         Car car2 = new Car();
         car2.setModelYear(2023);
         car2.setModelName("Tesla Model 3");
+        car2.setBodyShape("Sedan");
         car2.setMilesDriven(2316);
         car2.setCarColor("White");
         
